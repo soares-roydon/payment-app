0b9554db187c49236ec7b6556cb6a7a6163c7035
soares-roydon <soaresroydon@gmail.com>
2026-05-06 15:40:57 +0530

    Minor improvement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
