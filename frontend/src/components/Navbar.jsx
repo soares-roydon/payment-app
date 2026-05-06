6eb4b139f654782caa423c10c225a3c4b1ff37a8
soares-roydon <soaresroydon@gmail.com>
2026-05-06 17:34:58 +0530

    Correct image path

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export function Navbar() {
     <>
       <div className="flex justify-between px-5 py-3 mb-4 shadow-md">
         <div className="flex items-center gap-2">
-          <div className="size-8"><img src="../src/assets/logo.png" /></div>
+          <div className="size-8"><img src="/src/assets/logo.png" /></div>
           <div className="font-extrabold bg-clip-text text-transparent bg-linear-to-r from-violet-500 to-green-500">Payment-App</div>
         </div>
         <div>
