9da3f019a1285bdf74d2623341f0d53bd9975476
soares-roydon <soaresroydon@gmail.com>
2026-05-06 16:15:08 +0530

    Refresh balance at every second

--- frontend/src/components/BalanceCard.jsx
+++ frontend/src/components/BalanceCard.jsx
@@ -2,19 +2,21 @@ import { useEffect, useState } from "react";
 import { Card } from "../components/Card";
 
 export function BalanceCard() {
-  const [amount, setAmount] =useState(999)
+  const [balance, setBalance] =useState(999)
   let accountNo = "000123456789"
 
   useEffect(function () {
-    fetch("https://payment-app-6j79.onrender.com/api/v1/account/balance", {
-      method: "GET",
-      headers: {
-        "authorization": localStorage.getItem("token")
-      },
-    }).then(async function (response) {
-        const result = await response.json()
-        setAmount(result.balance)
-    })
+    setInterval(function () {
+      fetch("https://payment-app-6j79.onrender.com/api/v1/account/balance", {
+        method: "GET",
+        headers: {
+          "authorization": localStorage.getItem("token")
+        },
+      }).then(async function (response) {
+          const result = await response.json()
+          setBalance(result.balance)
+      })
+    }, 1000)
   }, [])
 
   return (
@@ -26,7 +28,7 @@ export function BalanceCard() {
             <div className="text-sm">{accountNo}</div>
             </div>
             <div className="bg-green-50 px-4 pt-2 text-blue-950">Balance</div>
-            <div className="bg-green-50 px-4 py-2 text-blue-950 font-medium text-2xl">${amount}</div>
+            <div className="bg-green-50 px-4 py-2 text-blue-950 font-medium text-2xl">${balance}</div>
           </Card>
         </div>
     </>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -10,6 +10,7 @@ export function Dashboard() {
   const [filter, setFilter] = useState("");
   const [users, setUsers] = useState([]);
   const [friends, setFriends] = useState([]);
+  const [balance, setBalance] = useState(999);
 
   const navigate = useNavigate();
 
@@ -22,16 +23,17 @@ export function Dashboard() {
   }, []);
 
   function search(value) {
-    fetch(`https://payment-app-6j79.onrender.com/api/v1/user/bulk?filter=${value}`, {
-      headers: {
-        authorization: localStorage.getItem("token")
-      }
-    }).then(
-      async function (response) {
-        const result = await response.json();
-        setUsers(result.user);
+    fetch(
+      `https://payment-app-6j79.onrender.com/api/v1/user/bulk?filter=${value}`,
+      {
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
       },
-    );
+    ).then(async function (response) {
+      const result = await response.json();
+      setUsers(result.user);
+    });
   }
 
   return (
@@ -46,7 +48,7 @@ export function Dashboard() {
         Log out
       </button>
       <div className="grid grid-cols-1 mx-4 mt-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        <BalanceCard />
+        <BalanceCard balance={balance} setBalance={setBalance}/>
       </div>
       <div className="mx-4 my-6 lg:grid grid-cols-2">
         <div className="flex flex-col">
@@ -69,7 +71,7 @@ export function Dashboard() {
         </div>
       </div>
       <div className="grid grid-cols-1 mb-5 md:grid-cols-2 lg:grid-cols-2">
-        <Table friends={friends} />
+        <Table friends={friends} setBalance={setBalance}/>
       </div>
     </>
   );
